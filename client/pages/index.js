e0e795f5015c70ffd75778fcd2fb9e836a67808e
hyemin23 <hm_stom@naver.com>
2021-04-30 14:34:43 +0900

    #section2 완료

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import React from "react";
-import styled from "styled-components";
-
-import { gsap } from 'gsap';
+import Sesctionone from "../components/SectionOne";
+import Sectionthree from "../components/sectionthree";
+import Sectiontwo from "../components/Sectiontwo";
 
 
 // register ScrollTrigger
@@ -15,80 +15,13 @@ const index = () => {
                 <title>메인페이지</title>
             </Head>
             {/* 첫 번째 섹션 */}
-            <SectionOne className="section showcase" >
-                <div className="showcase_img">
-                    <img src="images/mainfirst.jpg"></img>
-                </div>
-                <div className="showcase_event">
-                    <h1>NEW IN</h1>
-                    <h3>모든 상품 90%세일</h3>
-                </div>
-            </SectionOne>
-
+            <Sesctionone />
             {/* 두 번째 섹션 */}
-            <section>
-                <div>
-                    섹션2
-                </div>
-            </section>
-
-
+            <Sectiontwo />
             {/* 세 번째 섹션 */}
-            <section>
-                <div>
-                    섹션3
-                </div>
-            </section>
+            <Sectionthree />
         </div>
     )
-
 }
-
-const SectionOne = styled.section`
-
-    box-sizing : border-box;
-    margin:0;
-    padding : 0;
-    display : flex;
-    align-items : center;
-    justify-content : center;
-    height : 100vh;
-    overflow : hidden;
-
-    .showcase_img{
-        z-index:-1;
-       top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-    
-    img{
-      min-width: 100%;
-      min-height: 100%;
-      position: absolute;
-      top:50%;
-      left:50%;
-      transform: translate(-50%, -50%);
-      object-fit: cover;
-    }
-
-    &:after {
-      content: '';
-      position: absolute;
-      z-index: 1;
-      height: 100%;
-      width: 100%;
-      top: 0;
-      left: 0;
-      background: rgba(19, 18, 18, 0.5);
-    }
-
-    .showcase_event{
-        font-size : 90px;
-    }
-}
-`;
-
 
 export default index;
