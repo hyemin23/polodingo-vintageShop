0b18f6011fafe2e97156c9e4442a91391ff74e6a
hyemin23 <hm_stom@naver.com>
2021-05-10 17:12:41 +0900

    #order Screen 반응형 구현

--- client/pages/order/orderSheet/[id].js
+++ client/pages/order/orderSheet/[id].js
@@ -1,11 +1,9 @@
-import Link from "next/link"
-import styled from "styled-components"
-import { isMobile } from "../../../style/theme"
+import OrderSheetStyle from "../../../style/OrderSheetStyle"
 import OrderStep from "../../orderStep"
 
 const orderSheetScreen = () => {
   return (
-    <div style={{ margin: "auto", width: "100%" }}>
+    <div>
       <OrderStep />
       <OrderSheetStyle>
         <div className="order_title">
@@ -66,87 +64,5 @@ const orderSheetScreen = () => {
     </div>
   )
 }
-
-const OrderSheetStyle = styled.div`
-  max-width: 1500px;
-  margin: auto;
-  padding: 5rem 1rem;
-  text-align: center;
-
-  .order_title {
-    margin-bottom: 5rem;
-  }
-  .order_contents {
-    display: flex;
-    & > * {
-      flex-grow: 1;
-      flex-basis: 0;
-    }
-
-    h2 {
-      font-size: 16px;
-      font-weight: 400;
-    }
-
-    h3,
-    p {
-      text-align: start;
-    }
-
-    h3 {
-      font-weight: 400;
-      font-size: 16px;
-      border-bottom: 1px solid #ececec;
-    }
-
-    p {
-      padding: 30px;
-    }
-    .order_info {
-      & .order_items {
-        & .order_item_list {
-          display: flex;
-          align-items: center;
-          border-bottom: 1px solid #ececec;
-
-          & > *:not(img) {
-            flex-grow: 2;
-            flex-basis: 0;
-          }
-
-          img {
-            max-width: 100px;
-            margin: 1rem;
-          }
-        }
-      }
-    }
-  }
-
-  .order_summary {
-    h2 {
-      margin: 2rem 0;
-      color: #f3885f;
-    }
-    p {
-      font-size: 16px;
-      font-wight: 400;
-    }
-
-    & .order_summary_contents {
-      p {
-        padding: 0;
-        margin-bottom: 1rem;
-        text-align: center;
-      }
-    }
-    .btn {
-      width: 500px;
-    }
-  }
-
-  ${isMobile} {
-  }
-`
 
 export default orderSheetScreen
