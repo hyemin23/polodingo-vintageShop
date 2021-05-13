7c8b9a1366058f7240eb961c527d64e112f9e5c8
hyemin23 <hm_stom@naver.com>
2021-05-13 15:35:46 +0900

    #hydrate ssr 랜더링 설정

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,46 +1,20 @@
 import { HYDRATE } from 'next-redux-wrapper';
-
-const initialState = {
-  user: {
-    isLoggedIn: false,
-    user: null,
-    userId: 'test',
-    userPw: '1234',
-    userEmail: 'test@test.com',
-  },
-};
-
-// 액션 타입
-export const LOG_IN = 'LOG_IN';
-
-// 액션 생성자 함수 (action creator)
-export const loginAction = (data) => {
-  console.log('로그인 액션 들어옴');
-  return {
-    type: LOG_IN,
-    data,
-  };
-};
-
+import { combineReducers } from 'redux';
+import user from './user/userReducer';
+import post from './post/postReducer';
 // 인자 : (이전상태, 액션) => return : 다음상태 만들어줌
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      console.log('하이드레이트', action);
-      return { ...state, ...action.payload };
-    case LOG_IN:
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: true,
-          user: action.data,
-        },
-      };
-
-    default:
-      return state;
-  }
-};
-
+// 서버사이드 렌더링을 위해  HYDRATE에 index reducer 추가
+const rootReducer = combineReducers({
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        console.log('하이드레이트', action);
+        return { ...state, ...action.payload };
+      default:
+        return state;
+    }
+  },
+  user,
+  post,
+});
 export default rootReducer;
