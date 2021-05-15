06198e0189057d4385658830dd5dbeb474204f6b
kikio1101 <sadkop00@daum.net>
2021-05-15 23:06:55 +0900

    # 에러페이지 적용

---
+++ client/pages/404.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import { StockStyle } from '../style/StockStyle';
+export default function Error404() {
+  return (
+    <>
+      <StockStyle>
+        <Head>
+          <title>에러</title>
+        </Head>
+        <h2>404: 페이지를 찾을 수 없습니다.</h2>
+      </StockStyle>
+    </>
+  );
+}
