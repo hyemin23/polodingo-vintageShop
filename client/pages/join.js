dcc8d452e59db7982222bdf71842a101c0dfcb73
hyemin23 <hm_stom@naver.com>
2021-05-15 15:54:23 +0900

    #join validation 검증 추가

--- client/pages/join.js
+++ client/pages/join.js
@@ -1,47 +1,45 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
 import { SignFormStyle } from '../style/FormStyle';
 
 const join = () => {
-  const { register, handleSubmit, errors } = useForm({ mode: 'onChange' });
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const password = useRef();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  password.current = watch('userPw');
   const [validation, setValidation] = useState({
     value: true,
     message: '필수항목입니다.',
   });
 
+  console.log(errors);
+
   const onSubmit = (data) => {
     console.log(data);
+    console.log('서브밋');
   };
 
-  const msg = (_icon, _title, _timeSpeed) => {
-    Swal.fire({
-      icon: _icon,
-      titleText: _title,
-      customClass: {
-        popup: 'alrtForm',
-      },
-      timer: _timeSpeed,
-      timerProgressBar: true,
-    });
-  };
-
-  const onError = (e) => {
-    msg('error', e.userId.message, 2000);
-    console.log('onError 함수의 에러 내용');
-    console.log(e);
-  };
   return (
     <SignFormStyle>
       <div className="signForm_div">
         <h2>JOIN</h2>
-        <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form__content">
             <p>아이디</p>
             <input
               type="name"
+              name="userId"
               placeholder="Enter name"
               {...register('userId', {
                 required: validation,
@@ -52,13 +50,30 @@ const join = () => {
               })}
             />
           </div>
+          {errors.userId && errors.userId.type === 'required' && (
+            <p>{errors.userId.message}</p>
+          )}
+          {errors.userId && errors.userId.type === 'minLength' && (
+            <p>{errors.userId.message}</p>
+          )}
           <div className="form__content">
             <p>이메일</p>
             <input
               type="email"
               placeholder="Enter email"
-              {...register('userEmail')}
+              {...register('userEmail', {
+                required: true,
+                maxLength: 30,
+              })}
             />
+            <p>
+              {errors.userEmail && errors.userEmail.type === 'required' && (
+                <p>필수항목입니다</p>
+              )}
+              {errors.userEmail && errors.userEmail.type === 'maxLength' && (
+                <p>30자 내외로 입력해주세요.</p>
+              )}
+            </p>
           </div>
           <div className="form__content">
             <p>비밀번호</p>
@@ -73,14 +88,26 @@ const join = () => {
                 },
               })}
             />
+            {errors.userPw && errors.userPw.type === 'minLength' && (
+              <p>{errors.userPw.message}</p>
+            )}
           </div>
           <div className="form__content">
             <p>비밀번호 확인</p>
             <input
               type="password"
+              name="password2"
               placeholder="Confirm password"
-              {...register('userRpw')}
+              {...register('userRpw', {
+                required: true,
+                validate: (value) => {
+                  return value === password.current;
+                },
+              })}
             />
+            {errors.userRpw && errors.userRpw.type === 'validate' && (
+              <p>비밀번호가 일치하지 않습니다.</p>
+            )}
           </div>
           <div className="register_btn">
             <button type="submit" className="btn">
