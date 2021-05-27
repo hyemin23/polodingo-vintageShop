54e871f48901f2610d86df5d02d45dbc27ab2ccf
hyemin23 <hm_stom@naver.com>
2021-05-27 16:27:57 +0900

    nestjs 배포 test

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,27 +6,34 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule, { cors: true });
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
-  app.use(cookieParser());
+  try {
 
-  // app.enableCors();
-  // app.enableCors();
+  
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+    app.use(cookieParser());
 
-  //파일업로드를 위한 설정
-  // app.useStaticAssets(path.join(__dirname, '..', 'uploads'));
-  // app.use('/public', express.static(path.join(__dirname, '../public')));
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-  await app.listen(3000);
+    // app.enableCors();
+    // app.enableCors();
+
+    //파일업로드를 위한 설정
+    // app.useStaticAssets(path.join(__dirname, '..', 'uploads'));
+    // app.use('/public', express.static(path.join(__dirname, '../public')));
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    await app.listen(3000);
+  }
+  catch (error) {
+    throw new Error(error);
+  }
 }
 bootstrap();
